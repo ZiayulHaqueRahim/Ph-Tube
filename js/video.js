07ddd2dd91fbb54a1779f9e22acd8e8c77aaf3cf
Zia <ziair3812@gmail.com>
2024-10-06 04:29:04 +0600

    added main section

--- js/video.js
+++ js/video.js
@@ -1,5 +1,21 @@
-console.log('hello')
-
+// const cardDemo = {
+//     "category_id": "1003",
+//     "video_id": "aaaj",
+//     "thumbnail": "https://i.ibb.co/xgWL3vQ/kid-gorgeous.jpg",
+//     "title": "Kid Gorgeous",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/xsfkwN2/john.jpg",
+//             "profile_name": "John Mulaney",
+//             "verified": true
+//         }
+//     ],
+//     "others": {
+//         "views": "241K",
+//         "posted_date": ""
+//     },
+//     "description": "John Mulaney's 'Kid Gorgeous' has captured the hearts of many with 241K views. As a verified comedian, John delivers a masterclass in stand-up with clever anecdotes, quick wit, and relatable humor. This performance is a laugh-filled adventure through his unique take on life, politics, and pop culture."
+// }
 
 
 //1. fetch, load and show categories on html
@@ -14,29 +30,78 @@ console.log('hello')
         .then((data) => displayCategories(data.categories))
         .catch((error) => console.log(error));
 
+    };
+
+
+
+
+
+    //  create loadvideos
+    //create displayVideos
+
+    const loadVideos = () => {
+        fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then((res) => res.json())
+        .then((data) => displayVideos(data.videos))
+        .catch((error) => console.log(error))
     }
 
+    const displayVideos = (videos) => {
+        const videoContainer =document.getElementById('videoContainer')
+        videos.forEach(video => {
+            //creating a card
 
+            console.log(video);
+            const card = document.createElement('div');
+            card.classList = "card card-compact"
+            card.innerHTML = `
+                <figure class="h-[200px]">
+                    <img
+                    src=${video.thumbnail}
+                    class="w-full h-full object-cover"
+                    alt="Shoes" />
+                </figure>
+                <div class="px-0 py-2 flex gap-2">
+                   <div>
+                        <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture}>
+                   </div>
+                   <div>
+                        <h2 class="font-bold">
+                            ${video.title}
+                        </h2>
+                        <div class=" flex items-center gap-2">
+                            <p class="text-gray-400">${video.authors[0].profile_name}  </p>
+                            <img class=" w-4 h-4" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>
+                        </div>
+                   </div>               
+                </div>
+                `;
+
+                videoContainer.append(card)
+
+
+
+
+
+                
+        })
+    }
 
 
     const displayCategories = (categories) =>{
         const categoryContainer = document.getElementById('categories')
 
         categories.forEach(item => {
-        console.log(item);
+            //create button:
+            const button = document.createElement('button');
+            button.classList = 'btn';
+            button.innerText = item.category;
 
-        //"category_id": "1001",
-        //"category": "Music"
-
-        //create buttonK:
-        const button = document.createElement('button');
-        button.classList = 'btn';
-        button.innerText = item.category;
-
-        //add button to category container:
-        categoryContainer.append(button)
+            //add button to category container:
+            categoryContainer.append(button)
        })
 
     }
 
-    loadCategories()
+    loadCategories();
+    loadVideos();
